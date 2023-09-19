641136fc653d35ac43ec8c79ecd6e9fb2f6be68b
Aishu030 <aishuramesh031@gmail.com>
2023-09-19 18:49:58 +0530

    firstcommit

---
+++ Assignment6/Assignment6/Models/Assign6DbContext.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment6.Models;
+
+public partial class Assign6DbContext : DbContext
+{
+    public Assign6DbContext()
+    {
+    }
+
+    public Assign6DbContext(DbContextOptions<Assign6DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<BookAuthor> BookAuthors { get; set; }
+
+    public virtual DbSet<BookCategory> BookCategories { get; set; }
+
+    public virtual DbSet<Publisher> Publishers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:assign123.database.windows.net,1433;Initial Catalog=Assign6Db;User Id=Aishu030;Password=Aishudivi_030;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.AuthorId).HasName("PK__Author__70DAFC14BC9A4E5E");
+
+            entity.ToTable("Author");
+
+            entity.Property(e => e.AuthorId)
+                .ValueGeneratedNever()
+                .HasColumnName("AuthorID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.BookId).HasName("PK__Book__3DE0C227503ADBB9");
+
+            entity.ToTable("Book");
+
+            entity.Property(e => e.BookId)
+                .ValueGeneratedNever()
+                .HasColumnName("BookID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.PublisherId).HasColumnName("PublisherID");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Books)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__Book__CategoryID__628FA481");
+
+            entity.HasOne(d => d.Publisher).WithMany(p => p.Books)
+                .HasForeignKey(d => d.PublisherId)
+                .HasConstraintName("FK__Book__PublisherI__6383C8BA");
+        });
+
+        modelBuilder.Entity<BookAuthor>(entity =>
+        {
+            entity.HasKey(e => e.BookAuthorId).HasName("PK__BookAuth__21B24F3923C9CB8E");
+
+            entity.ToTable("BookAuthor");
+
+            entity.Property(e => e.BookAuthorId)
+                .ValueGeneratedNever()
+                .HasColumnName("BookAuthorID");
+            entity.Property(e => e.AuthorId).HasColumnName("AuthorID");
+            entity.Property(e => e.BookId).HasColumnName("BookID");
+
+            entity.HasOne(d => d.Author).WithMany(p => p.BookAuthors)
+                .HasForeignKey(d => d.AuthorId)
+                .HasConstraintName("FK__BookAutho__Autho__6754599E");
+
+            entity.HasOne(d => d.Book).WithMany(p => p.BookAuthors)
+                .HasForeignKey(d => d.BookId)
+                .HasConstraintName("FK__BookAutho__BookI__66603565");
+        });
+
+        modelBuilder.Entity<BookCategory>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__BookCate__19093A2B77428BA0");
+
+            entity.ToTable("BookCategory");
+
+            entity.Property(e => e.CategoryId)
+                .ValueGeneratedNever()
+                .HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Publisher>(entity =>
+        {
+            entity.HasKey(e => e.PublisherId).HasName("PK__Publishe__4C657E4BB2F6E326");
+
+            entity.ToTable("Publisher");
+
+            entity.Property(e => e.PublisherId)
+                .ValueGeneratedNever()
+                .HasColumnName("PublisherID");
+            entity.Property(e => e.Address)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PublisherName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
